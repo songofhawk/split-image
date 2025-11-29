4e9a7285822853f038ae1b25496d25f37d61bed8
helix.song <helix.song@vinotech.com>
2025-11-29 12:48:00 +0800

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -17,7 +16,7 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': '/src',
       }
     }
   };
